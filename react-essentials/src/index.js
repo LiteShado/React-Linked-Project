3d92d1a4d6f1b7b11d9dfc71d6179fae1283d11b
Natalie <theliteshado@icloud.com>
2021-11-20 00:40:30 -0500

    start over

---
+++ react-essentials/src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import reportWebVitals from './reportWebVitals';
+
+ReactDOM.render(
+  React.createElement("h1", null, "Hey!"),
+  document.getElementById('root')
+);
